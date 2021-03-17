2e5008759e54f08a909fb4edcbe3ccf51c0199f4
Mayank Khare <mayankkhare92@gmail.com>
2021-03-18 00:08:43 +0530

    App developed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<div className="header-container center-div">
+			<div className="logo-container">
+				<a href="/"><img src="../spacex-logo.png" alt="spacex-logo" /></a>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
